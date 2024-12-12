fb86ef759257ff40b71c7b67d47c7254e01704f6
Dan <dan@syzygyx.com>
2024-12-12 15:52:42 -0700

    Added version.py

---
+++ version.py
@@ -0,0 +1,24 @@
+"""
+MCC Version Information
+
+This file tracks version information for MCT and includes patch data
+for updating between versions.
+"""
+
+# Current version
+__version__ = "0.2.20241212_1525"
+
+# Previous version (for patching)
+__previous_version__ = "0.2.20241211_1430"
+
+# Patch data (if available)
+patch_data = """
+diff --git a/version.py b/version.py
+index abc..def 100644
+--- a/version.py
++++ b/version.py
+@@ -1,3 +1,3 @@
+-__version__ = "0.2.20241211_1430"
++__version__ = "0.2.20241212_1525"
+...
+"""
